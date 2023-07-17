ed6e7d3f8deb93db53088b5f584b2d5bb6d5453c
Melon-Lo <dp98062@gmail.com>
2023-07-17 23:37:35 +0800

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from 'components/Header';
 import Checkout from 'pages/Checkout';
+import Footer from 'components/Footer';
 
 export default function App() {
   return (
     <>
       <Header />
       <Checkout />
+      <Footer />
     </>
   );
 }
