1755c2aed1bd1e99fa2168616ab918058fe091fa
Melon Lo <dp98062@gmail.com>
2023-07-29 15:44:24 +0800

    Fix bugs

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,14 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import initailData from "components/Cart/itemsData";
+import { ShippingContext } from "./ShippingContext";
 export const CartContext = createContext()
 
 export function CartContextProvider({ children }) {
   const [currentItems, setCurrentItems] = useState(initailData)
+  const { shipping } = useContext(ShippingContext)
 
   let itemTotal = []
   currentItems.map(item => {
     itemTotal.push(item.price * item.quantity)
   })
+  itemTotal.push(shipping)
 
   const total = itemTotal.reduce((accumulator, currenValue) => {
     return accumulator + currenValue
