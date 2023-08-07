838769e822be181e32688fc86785fc41d673ed3e
Melon Lo <dp98062@gmail.com>
2023-08-08 01:46:22 +0800

    Finish Scss

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import './App.css';
 import Header from 'components/Header/Header';
 import Checkout from 'pages/Checkout/Checkout';
 import Footer from 'components/Footer/Footer';
-import { useContext } from 'react';
-import { ThemeContext } from 'context/ThemeContext';
 
 export default function App() {
-  const { theme } = useContext(ThemeContext)
   return (
     <div className="app">
       <Header />
